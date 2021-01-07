e5862b11230228312c54405d2f45b294da388338
GupCa <anntytova@gmail.com>
2021-01-07 22:45:57 +0200

    Revert "asdas"
    This reverts commit 2f19e2e1c3d9dc2c69ca283ff80f012f77dd4db3.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ErrorComponent } from './error/error.component';
 import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { SocialLoginComponent } from './social-login/social-login.component';
 
@@ -17,13 +17,9 @@ import { SocialLoginComponent } from './social-login/social-login.component';
     ErrorComponent,
     RegistrationComponent,
     SocialLoginComponent,
+    LogoutComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-
-import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,29 +6,18 @@ import { AuthService } from '../service/auth.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  loginForm = this.fb.group({
-    name: [''],
-    password: [''],
-  });
+  // loginForm = this.fb.group({
+  //   name: [''],
+  //   password: [''],
+  // });
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {}
+  // constructor(private fb: FormBuilder, private authService: AuthService) {}
 
+  constructor() {}
   ngOnInit(): void {}
 
   onSubmit(): void {
-    // window.location.replace(
-    //   `http://localhost:8080/auth/login?username=admin&password=admin`
-    // );
     console.log('onSubmit called');
-
-    const formData = new FormData();
-
-    formData.append('username', this.loginForm.value.name);
-    formData.append('password', this.loginForm.value.password);
-
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:8080/auth/login');
-    xhr.send(formData);
 
     // this.authService.login(
     //   this.loginForm.value.name,
